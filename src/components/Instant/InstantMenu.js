bbe11d6cc31fd397b93fa7f8d61178858c5d186a
Atticus Pomerantz <rapomerantz@gmail.com>
2018-04-30 14:44:54 -0500

    switched to select

--- src/components/Instant/InstantMenu.js
+++ src/components/Instant/InstantMenu.js
@@ -9,13 +9,32 @@ const mapStateToProps = state => ({
 });
 
 class InstantMenu extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            anchorEl: null,
+        };
+      }
 
+    //handle menu close
+  closeMenu = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  openMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
     
 
 render() {
 
+    const { anchorEl } = this.state;
+
+
   return (
     <div>
+                    <Button variant="raised" color="primary" className="buttonWire" onClick={this.openMenu}> Select Device </Button>
+
         <Menu id="deviceSelect"
                 anchorEl = {anchorEl}
                 open={Boolean(anchorEl)}
