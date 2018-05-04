fa32b193ad8498ddd81902fe96e433743ce23b56
Atticus Pomerantz <rapomerantz@gmail.com>
2018-05-04 13:40:56 -0500

    user top drawer working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,25 @@ import './Header.css'
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 import { triggerLogout } from '../../redux/actions/loginActions';
 
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Button from 'material-ui/Button';
+import Icon from 'material-ui/Icon';
+import Drawer from 'material-ui/Drawer';
+
+
 const mapStateToProps = state => ({
   state
 });
 
 class Header extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      top: false
+    }
+  }
 
   componentDidMount() {
     this.props.dispatch({ type: USER_ACTIONS.FETCH_USER });
@@ -20,13 +34,42 @@ class Header extends Component {
     // this.props.history.push('home');
   }
 
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+  
   render() {
+
+    const topDrawer = (
+        <div>
+        <AppBar position="static">
+            <p>
+              <Button size="small" id="appBarButton" > Username: {this.props.state.user.userName} </Button>                
+              <Button size="small" id="logoutButton" onClick={this.logout}> Log Out </Button>         
+            </p>
+        </AppBar>
+      </div>
+    )
+
     return(
-      <div className="instructions">
-                <p>
-                    Username: {this.props.state.user.userName}
-                    <button id="logoutButton" onClick={this.logout}> Log Out </button>         
-                </p>
+      <div className="navbar">
+
+        <Button onClick={this.toggleDrawer('top', true)}>
+          User <Icon>keyboard_arrow_down</Icon>
+        </Button>
+
+        <Drawer anchor="top" open={this.state.top} onClose={this.toggleDrawer('top', false)}>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer('top', false)}
+            onKeyDown={this.toggleDrawer('top', false)}>
+            {topDrawer}
+          </div>
+        </Drawer>
       </div>
     )
   }
